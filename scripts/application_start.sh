b6efeac45240a932c7936554cf82047e5951d42e
AlanRodriguezGomez <47120912+AlanRodriguezGomez@users.noreply.github.com>
2022-10-23 05:09:06 -0500

    Cambios
    * se agregan cambios

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,7 +2,7 @@
 cd /home/ec2-user/app-frontend
 npm run build
 pm2 install typescript
-pm2 start npm --name "next" -- start
+pm2 start npm --name "nextapp" -- start
 pm2 startup
 pm2 save
 pm2 restart all
